14978318002f6c2c768fd88b36982411ae2c6441
Maria Mariana Cagnoni <mariamariana.cagnoni@gmail.com>
2021-05-06 20:28:17 -0300

    server

---
+++ server.py
@@ -0,0 +1,49 @@
+import socket         #socket for the comunication.
+import threading    #alloy run multiple tasks  at the same time
+
+HOST = '192.168.0.1'
+PORT = 555555
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)        #AF_INET = IPV4   and SOCK_STREAM = TCP.
+server.bind((HOST,PORT))
+
+
+server.listen()
+clients = []                    #empty list for clients
+nickNames = []                  #empty list for nickNames
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f'{nicknames[clients.index(client)]} says {message}')
+            broadcast(message)
+        except:
+            index = clients.index(client)
+
+
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"connected with{str(address)}")
+
+        client.send("NICK".encode('UTF-8'))
+        nickname = client.recv(1024)                                    #in bytes
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'The nickname of the client is:{nickname}')
+        broadcast(f"{nickname}join the chat!\n ".encode('UTF-8'))
+        client.send("connect to the chat!".encode('UTF-8'))
+
+        thread = threading.Thread(target = handle , args =(client,))
+        thread.start()
+
+
+
+
